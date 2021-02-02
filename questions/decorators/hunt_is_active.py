f9cec925eeda46c4bc51fb55c344408f4386e299
Gaurav Pandey <gauravguitarrocks@gmail.com>
2021-02-02 12:32:24 +0530

    hunt: Update hunt_is_active functionality.

--- questions/decorators/hunt_is_active.py
+++ questions/decorators/hunt_is_active.py
@@ -4,16 +4,27 @@ from django.shortcuts import render
 from django.utils import timezone
 import pytz
 
+PUBLISH_DATE = datetime(year=2021, month=2, day=11, hour=0, minute=0, second=0)
+DEACTIVATE_DATE = datetime(year=2021, month=2, day=12, hour=0, minute=0, second=0)
+
 def hunt_is_active(function):
     def wrap(request, *args, **kwargs):
-        tz = pytz.timezone('Asia/Kolkata')
-        publish_date = tz.localize(datetime(year=2021, month=2, day=1, hour=20, minute=45, second=0))
 
-        if tz.localize(datetime.now())>=publish_date:
-            print("Early")
+        tz = pytz.timezone('Asia/Kolkata')
+        local_publish_date = tz.localize(PUBLISH_DATE)
+        local_deactivate_date = tz.localize(DEACTIVATE_DATE)
+        local_current_date = tz.localize(datetime.now())
+        cur_user = request.user.username
+
+        if cur_user == "admin":
             return function(request, *args, **kwargs)
         else:
-            return render(request, 'wait.html', {"time": publish_date})
-    wrap.__doc__ = function.__doc__
-    wrap.__name__ = function.__name__
+            if ((local_current_date >= local_publish_date) and (local_current_date <= local_deactivate_date)):
+                return function(request, *args, **kwargs)
+
+        return render(request, 'wait.html', {"publish_time": local_publish_date, "deactivate_time": local_deactivate_date})
+
+    wrap.__doc__ = getattr(function, "__doc__", "")
+    wrap.__name__ = getattr(function, "__name__", "")
+
     return wrap
