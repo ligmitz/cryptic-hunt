b9ee868486f768cb99f7aad2a36468d092daffe0
Rohan Nawathe <rohannawathe@hotmail.com>
2021-02-11 04:23:19 +0530

    Updated installer script

--- install.sh
+++ install.sh
@@ -1,23 +1,22 @@
 #!/bin/bash
+cd "$(dirname "$0")"
+
 WORKING_DIR="/opt/iste/abhedya"
 
-if [ -d "$WORKING_DIR" ]; then
-    echo "Removing Previous Installation";
-    rm -Rf $WORKING_DIR;
-fi
-
-cp * $WORKING_DIR
+cp -rf . -t $WORKING_DIR
 
 echo "Copying the service file"
 cp abhedya.service /etc/systemd/system/
 
-echo "Enabeling the service"
+cd $WORKING_DIR
+docker-compose up -d -f docker/docker-compose.yml
+docker-compose exec app python manage.py questions/fixtures/questions.json
+docker-compose down
+
+echo "Enabling the service"
 systemctl enable abhedya.service
 
 echo "Starting the service"
 systemctl start abjedya.service
 
-cd $WORKING_DIR
 
-docker-compose up -d -f docker/docker-compose.yml
-docker-compose exec app python manage.py questions/fixtures/questions.json
