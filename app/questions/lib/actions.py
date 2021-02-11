a08e486bc638dbf3018f793f149356ec858e0866
Parth Pant <parthpant4@gmail.com>
2021-02-11 10:45:43 +0530

    changed the active date to 13th

--- app/questions/lib/actions.py
+++ app/questions/lib/actions.py
@@ -1,8 +1,8 @@
 from datetime import datetime
 import pytz
 
-PUBLISH_DATE = datetime(year=2021, month=2, day=11, hour=0, minute=0, second=0)
-DEACTIVATE_DATE = datetime(year=2021, month=2, day=11, hour=23, minute=59, second=59)
+PUBLISH_DATE = datetime(year=2021, month=2, day=13, hour=0, minute=0, second=0)
+DEACTIVATE_DATE = datetime(year=2021, month=2, day=13, hour=23, minute=59, second=59)
 tz = pytz.timezone('Asia/Kolkata')
 local_publish_date = tz.localize(PUBLISH_DATE)
 local_deactivate_date = tz.localize(DEACTIVATE_DATE)
