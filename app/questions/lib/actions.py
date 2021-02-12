b07f289a729384dbdf497c2f63b8a50634760287
Parth Pant <parthpant4@gmail.com>
2021-02-13 00:11:58 +0530

    utc

--- app/questions/lib/actions.py
+++ app/questions/lib/actions.py
@@ -1,9 +1,9 @@
 from datetime import datetime
 import pytz
 
-PUBLISH_DATE = datetime(year=2021, month=2, day=13, hour=0, minute=0, second=0)
-DEACTIVATE_DATE = datetime(year=2021, month=2, day=13, hour=23, minute=59, second=59)
-tz = pytz.timezone('Asia/Kolkata')
+PUBLISH_DATE = datetime(year=2021, month=2, day=12, hour=18, minute=0, second=0)
+DEACTIVATE_DATE = datetime(year=2021, month=2, day=13, hour=18, minute=59, second=59)
+tz = pytz.timezone('UTC')
 local_publish_date = tz.localize(PUBLISH_DATE)
 local_deactivate_date = tz.localize(DEACTIVATE_DATE)
 local_current_date = tz.localize(datetime.now())
