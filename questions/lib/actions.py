67b7f228a5d9415f6ca780460103bca5af91c222
Rohan Nawathe <rohannawathe@hotmail.com>
2021-12-29 19:10:30 +0530

    Fixed time check logic

--- questions/lib/actions.py
+++ questions/lib/actions.py
@@ -1,12 +1,18 @@
 from datetime import datetime
 import pytz
 
+utc_tz = pytz.utc
+tz = pytz.timezone('Asia/Kolkata')
+
+
+# These should be in IST
 PUBLISH_DATE = datetime(year=2021, month=12, day=29, hour=18, minute=00, second=00)
 DEACTIVATE_DATE = datetime(year=2021, month=12, day=31, hour=23, minute=59, second=59)
-tz = pytz.timezone('Asia/Kolkata')
+
+
 local_publish_date = tz.localize(PUBLISH_DATE)
 local_deactivate_date = tz.localize(DEACTIVATE_DATE)
-local_current_date = tz.localize(datetime.now())
+local_current_date = tz.localize(utc_tz.localize(datetime.utcnow()))
 
 def is_active_period():
 
