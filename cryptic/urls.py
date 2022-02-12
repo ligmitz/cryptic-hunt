056ffd3083aab8525837b8d97639c7db7a88348c
lucifer1708 <20bce091@nith.ac.in>
2022-02-12 23:02:55 +0530

    Badhai ho chl pda sb

--- cryptic/urls.py
+++ cryptic/urls.py
@@ -36,7 +36,8 @@ urlpatterns = [
     path("password-reset/done/", auth_views.PasswordResetDoneView.as_view(template_name='user/password_reset_done.html'), name="password_reset_done"),
     path("password-reset-confirm/<uidb64>/<token>/", auth_views.PasswordResetConfirmView.as_view(template_name='user/password_reset_confirm.html'), name="password_reset_confirm"),
     path("password-reset-complete/", auth_views.PasswordResetCompleteView.as_view(template_name='user/password_reset_complete.html'), name="password_reset_complete"),
-    path('emailVerification/<uidb64>/<token>',activate, name='emailActivate')
+    path('emailVerification/<uidb64>/<token>',activate, name='emailActivate'),
+    path('activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/',activate, name='activate'),  
 ]
 
 
